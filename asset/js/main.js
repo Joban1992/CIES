772b9d6b6924e422b09fb65c45b2218abe7983a3
Joban1992 <singh.jobanpreet@tcs.com>
2016-07-05 11:16:50 +0530

    Add files via upload

---
+++ asset/js/main.js
@@ -0,0 +1,54 @@
+$(function() {
+    $(".btn-menu").click(function() {
+        $(".menu-items").slideToggle();
+    });
+    $(document).click(function(event) {
+        if (!$(event.target).is('.menu-items') && !$(event.target).is('.btn-menu') && !$(event.target).is('.btn-menu span')) {
+            if ($('.menu-items').is(":visible")) {
+                $('.menu-items').slideToggle();
+            }
+        }
+        if (!$(event.target).is('.menu-wrap') && !$(event.target).is('.btn-mnu') && !$(event.target).is('.btn-mnu span')) {
+            if ($('.menu-wrap').is(":visible")) {
+                if ($('.fixed-top').hasClass("full-height")) {
+                    $('.fixed-top .menu-wrap').slideUp(function() {
+                        $('.fixed-top').removeClass("full-height");
+                        $('body').css('overflow', 'visible');
+                    });
+                }
+            }
+        }
+    });
+    $(".btn-big-menu,.close-menu").click(function() {
+        $(".big-menu,.big-menu-bar").toggleClass("hidden");
+        $('body').toggleClass('hide-overflow');
+    });
+    $('.btn-mnu').click(function() {
+        if (!$('.fixed-top').hasClass("full-height")) {
+            $('.fixed-top').addClass("full-height");
+            $('.fixed-top').removeClass("opaq");
+            $('.fixed-top .menu-wrap').slideDown();
+            $('body').css('overflow', 'hidden');
+        } else {
+            $('.fixed-top .menu-wrap').slideUp(function() {
+                $('.fixed-top').removeClass("full-height");
+                $('body').css('overflow', 'visible');
+            });
+        }
+    });
+    $(document).scroll(function() {
+        if ($('body').scrollTop() >= 100) {
+            $('body').addClass('smallHeader');
+        } else {
+            $('body').removeClass('smallHeader');
+        }
+    });
+    $(".goto").click(function() {
+//        $("#" + $(this).attr("data-go")).ScrollTo({
+//            duration: 1000,
+//            easing: 'linear'
+//        });
+        var data = $(this).attr("data-redirect");
+        window.location = data;
+    })
+});
